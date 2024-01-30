bc6d55f68ca2e698e8f244aa746dbec579bf8e4b
shuntaaaa247 <juntairenzu3@gmail.com>
2024-01-30 19:44:45 +0900

    フロントエンドの環境構築

---
+++ frontend/src/pages/login/Login.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.css";
+import Topbar from "../../components/topbar/Topbar";
+
+export default function Home() {
+  return (
+    <div className="homeContainer">
+      <Topbar />
+    </ div>
+  )
+}
