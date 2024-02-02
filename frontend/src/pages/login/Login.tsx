cbe0c4944113794c09c54a52025d9a53b42865df
shuntaaaa247 <juntairenzu3@gmail.com>
2024-02-03 01:18:04 +0900

    TailwindCSS適用

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -27,29 +27,61 @@ export default function Login() {
     }
   }
   return (
-    <div className="login">
-      <div className="loginWrapper">
-        <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}>
-        <input type="email" placeholder="Eメール" required ref={email}/>
-        <input type="password" placeholder="パスワード" required ref={password}/>
-        <button>ログイン</button>
-        {JSON.stringify(authState)}
-        </form>
-        <Link to="/register">
-          <span className="sidebarListItemText">サインアップページ</span>
-        </Link>
-        <br/>
-        { authState.user 
-          ? <Link to="/">
-              <span className="">ホームへ</span>
-            </Link> 
-          : <></>
-        }
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 mx-[33%] translate-y-32 shadow-2xl rounded-2xl">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-3xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST" onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  ref={email}
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  ref={password}
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 mt-[15%] mb-[3%] text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-      
-    </ div>
-
-
-
   )
 }
--- frontend/src/pages/register/Register.tsx
+++ frontend/src/pages/register/Register.tsx
@@ -3,7 +3,6 @@ import { AuthContext } from "../../state/AuthContext";
 import { useContext } from "react";
 import axios from "axios";
 import { loginCall } from "../../state/actionCalls";
-import { Link } from "react-router-dom";
 
 interface INewUserInfo {
   username: string,
@@ -46,26 +45,100 @@ export default function Register () {
 
 
   return(
-    <div className="register">
-      <div className="registerWrapper">
-        <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}>
-          <input type="text" placeholder="ユーザーネーム" required ref={ username }/>
-          <input type="email" placeholder="Eメール" required ref={ email } />
-          <input type="password" placeholder="パスワード" required ref={ password } />
-          <button>サインアップ</button>
-          {JSON.stringify(authState)}
-        </form>
-        <Link to="/login">
-          <span className="">ログインページ</span>
-        </Link>
-        <br/>
-        { authState.user 
-          ? <Link to="/">
-              <span className="">ホームへ</span>
-            </Link> 
-          : <></>
-        }
+    // <div className="register">
+    //   <div className="registerWrapper">
+    //     <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}>
+    //       <input type="text" placeholder="ユーザーネーム" required ref={ username }/>
+    //       <input type="email" placeholder="Eメール" required ref={ email } />
+    //       <input type="password" placeholder="パスワード" required ref={ password } />
+    //       <button>サインアップ</button>
+    //       {JSON.stringify(authState)}
+    //     </form>
+    //     <Link to="/login">
+    //       <span className="">ログインページ</span>
+    //     </Link>
+    //     <br/>
+    //     { authState.user 
+    //       ? <Link to="/">
+    //           <span className="">ホームへ</span>
+    //         </Link> 
+    //       : <></>
+    //     }
+    //   </div>
+    // </div>
+  
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 mx-[33%] translate-y-32 shadow-2xl rounded-2xl">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-3xl font-bold leading-9 tracking-tight text-gray-900">
+            Register your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST" onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}>
+          <div>
+              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                Username
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  ref={username}
+                />
+              </div>
+            </div>
+            
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  ref={email}
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  ref={password}
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 mt-[15%] mb-[3%] text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
+        {/* {JSON.stringify(authState)} */}
       </div>
-    </div>
   )
 }
