cc51bc27b3b44530dc51ecc7977f42542303c171
shuntaaaa247 <juntairenzu3@gmail.com>
2024-02-03 23:41:11 +0900

    誤字修正

--- frontend/src/state/actionCalls.ts
+++ frontend/src/state/actionCalls.ts
@@ -7,7 +7,7 @@ interface ILoginCallParams {
 
 export const loginCall = async (user: ILoginCallParams, dispatch: React.Dispatch<any>) => {
   console.log("loginCallが呼ばれました")
-  dispatch({ type: "LOGIN START" });
+  dispatch({ type: "LOGIN_START" });
   try {
     const res = await axios.post("/auth/login", user);
     dispatch({type: "LOGIN_SUCCESS", payload: res.data});
