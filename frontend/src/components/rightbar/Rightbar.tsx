4369053d36b8ac9ff7aa0b3561684e5a15fadd4f
shuntaaaa247 <juntairenzu3@gmail.com>
2024-02-10 19:37:14 +0900

    RightbarのUI作成(途中)

--- frontend/src/components/rightbar/Rightbar.tsx
+++ frontend/src/components/rightbar/Rightbar.tsx
@@ -3,85 +3,52 @@ import "./Rightbar.css"
 export default function Rightbar() {
   return(
     <div className="Rightbar">
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
-      <h1>Rightbar</h1>
+      {/* Trands totrack */}
+      <div className="w-[90%] bg-stone-100 rounded-3xl ml-auto mr-auto mt-[5%]">
+        <p className="text-3xl font-semibold pt-4 pl-5">Trends to tranck</p>
+        <ul className="ml-[8%] pb-5">
+          <li className="my-2">
+            <span className="text-sm text-stone-600">1・Trend</span>
+            <br />
+            <span className="text-lg">TypeScript</span>
+          </li>
+          <li className="my-2">
+            <span className="text-sm text-stone-600">2・Trend</span>
+            <br />
+            <span className="text-lg">Python</span>
+          </li>
+          <li className="my-2">
+            <span className="text-sm text-stone-600">3・Trend</span>
+            <br />
+            <span className="text-lg">Full Stack</span>
+          </li>
+          <li className="my-2">
+            <span className="text-sm text-stone-600">4・Trend</span>
+            <br />
+            <span className="text-lg">Node.js</span>
+          </li>
+          <li className="my-2">
+            <span className="text-sm text-stone-600">5・Trend</span>
+            <br />
+            <span className="text-lg">T3 Stack</span>
+          </li>
+        </ul>
+      </div>
 
+      {/* Who to follow */}
+      <div className="">
+        <ul>
+          <li>
+            山田太郎
+          </li>
+          <li>
+            山田太郎
+          </li>
+          <li>
+            山田太郎
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
