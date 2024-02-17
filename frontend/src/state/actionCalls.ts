d2acb3cf5e5d108251f311dfd32f007008a9739a
shuntaaaa247 <juntairenzu3@gmail.com>
2024-02-18 01:14:02 +0900

    本番環境のエラー分析

--- frontend/src/state/actionCalls.ts
+++ frontend/src/state/actionCalls.ts
@@ -9,7 +9,8 @@ export const loginCall = async (user: ILoginCallParams, dispatch: React.Dispatch
   console.log("loginCallが呼ばれました")
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post("/auth/login", user);
+    // const res = await axios.post("/auth/login", user);
+    const res = await axios.post("https://my-mern-sns-api.onrender.com/auth/login", user);
     dispatch({type: "LOGIN_SUCCESS", payload: res.data});
     console.log("loginCall成功");
     console.log(res.data);
